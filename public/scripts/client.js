4c1069e9e93f8346be5ca950b3b90c97268f84d6
OdeliaFink <odelia.finkel@gmail.com>
2021-02-24 21:14:08 +0000

    Updated Dynamic Functions, Index Doc to Remove Container and Restyled CSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac',
+    },
+    content: {
+      text:
+        'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd',
+    },
+    content: {
+      text: 'Je pense , donc je suis',
+    },
+    created_at: 1461113959088,
+  },
+];
+
 const tweetData = {
   user: {
     name: 'Newton',
@@ -18,7 +44,7 @@ const tweetData = {
 
 const createTweetElement = function (tweetData) {
   //put in the html for the entire tweet
-  const $tweet = $('article').addClass('tweet');
+  const $tweet = $('<article>').addClass('tweet');
   const html = `<header>
   <div class="user-info">
     <img src=${tweetData.user.avatars}> 
@@ -42,3 +68,15 @@ const createTweetElement = function (tweetData) {
   const result = $tweet.append(html);
   return result;
 };
+
+const renderTweets = function (tweets) {
+  const container = $('.tweet-container').html('');
+  console.log(container);
+  tweets.forEach((tweet) => {
+    const tweetElement = createTweetElement(tweet);
+    container.prepend(tweetElement);
+  });
+};
+
+$(document).ready(() => renderTweets(data));
+//once doc is loaded then render function
