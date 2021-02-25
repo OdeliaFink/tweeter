cf68152d6e634d2c1f598b6a9b004d700710b5db
OdeliaFink <odelia.finkel@gmail.com>
2021-02-25 00:11:04 +0000

    Updated and Refactored To Fetch Tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,33 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const data = [
-  {
+$(document).ready(() => {
+  const data = [
+    {
+      user: {
+        name: 'Newton',
+        avatars: 'https://i.imgur.com/73hZDYK.png',
+        handle: '@SirIsaac',
+      },
+      content: {
+        text:
+          'If I have seen further it is by standing on the shoulders of giants',
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: 'Descartes',
+        avatars: 'https://i.imgur.com/nlhLi3I.png',
+        handle: '@rd',
+      },
+      content: {
+        text: 'Je pense , donc je suis',
+      },
+      created_at: 1461113959088,
+    },
+  ];
+  const tweetData = {
     user: {
       name: 'Newton',
       avatars: 'https://i.imgur.com/73hZDYK.png',
@@ -16,91 +40,64 @@ const data = [
         'If I have seen further it is by standing on the shoulders of giants',
     },
     created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-];
+  };
 
-const tweetData = {
-  user: {
-    name: 'Newton',
-    avatars: 'https://i.imgur.com/73hZDYK.png',
-    handle: '@SirIsaac',
-  },
-  content: {
-    text: 'If I have seen further it is by standing on the shoulders of giants',
-  },
-  created_at: 1461116232227,
-};
+  const createTweetElement = function (tweetData) {
+    let $tweet = $('<article>').addClass('tweet');
 
-const createTweetElement = function (tweetData) {
-  //put in the html for the entire tweet
-  const $tweet = $('<article>').addClass('tweet');
-  const html = `<header>
-  <div class="user-info">
-    <img src=${tweetData.user.avatars}> 
-    <h1>${tweetData.user.name}</h1>
-   </div>
-   <div>
-    <h2>${tweetData.user.handle}</h2>
-   </div>
-</header>
- <div class="tweet-text">
-  <p>
-    ${tweetData.content.text}
-  </p>
-</div>
-<footer>
-  <p>
-    ${tweetData.created_at}
-    </p>
-</footer>
-`;
-  const result = $tweet.append(html);
-  return result;
-};
+    let html = `<header>
+      <div class="user-info">
+        <img src=${tweetData.user.avatars}> 
+        <h1>${tweetData.user.name}</h1>
+       </div>
+       <div>
+        <h2>${tweetData.user.handle}</h2>
+       </div>
+    </header>
+     <div class="tweet-text">
+      <p>
+        ${tweetData.content.text}
+      </p>
+    </div>
+    <footer>
+      <p>
+        ${tweetData.created_at}
+        </p>
+    </footer>
+    `;
+    let result = $tweet.append(html);
+    return result;
+  };
+  const renderTweets = function (tweets) {
+    const container = $('.tweet-container').html('');
+    tweets.forEach((tweet) => {
+      const tweetElement = createTweetElement(tweet);
+      container.prepend(tweetElement);
+      console.log(tweetElement);
+    });
+  };
 
-const renderTweets = function (tweets) {
-  const container = $('.tweet-container').html('');
-  tweets.forEach((tweet) => {
-    const tweetElement = createTweetElement(tweet);
-    container.prepend(tweetElement);
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    console.log($('form').serialize());
+
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $('form').serialize(),
+    }).then((res) => console.log(res));
   });
-};
 
-$(document).ready(() => renderTweets(data));
-//once doc is loaded then render function
+  const loadTweets = function () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'JSON',
+    }).then((results) => {
+      console.log(results);
+      renderTweets(results);
+    });
+  };
 
-$('form').on('submit', (event) => {
-  event.preventDefault();
-  console.log($('form').serialize());
-
-  $.ajax({
-    url: '/tweets',
-    method: 'POST',
-    data: $('form').serialize(),
-  }).then((res) => console.log(res));
+  loadTweets();
 });
-
-const loadTweets = function () {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    dataType: 'JSON',
-  })
-  .then((results) => {
-    console.log(results);
-    renderTweets(results);
-  });
-};
-
-loadTweets();
